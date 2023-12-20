25e72064efd2c8c48855615a0b4327828bbcc8ef
imaalchoholic <bahdassas@gmail.com>
2023-12-20 19:33:39 +0800

    shit

--- vark vark vark/functiontextzwei.js
+++ vark vark vark/functiontextzwei.js
@@ -1,39 +1,51 @@
-let j = 0;
-function addelement() {
-  body = document.createElement("div");
-  content = document.createTextNode(j);
-  body.style.cssText = "width: 100px; height: 100px; background-color: red;";
-  body.appendChild(content);
-  document.getElementById("boss").appendChild(body);
-  j++;
-}
-for (i = 0; i <= 12; i++) {
-  addelement();
-}
-function changecolor() {
-  change = document.getElementById("boss");
-  boss.style.cssText = "background-color: green";
-}
-var num1 = document.getElementById("num1");
-var num2 = document.getElementById("num2");
-var input = document.getElementById("input");
-function check() {
-  if (parseInt(num1.innerText) + parseInt(num2.innerText) == input.value) {
-    alert("Correct");
+const keypress = document.getElementsByClassName("container");
+const lober = document.getElementsByClassName("lober")[0];
+var bark = new Audio("barkx2.wav");
+var whistle = new Audio("whistle.wav");
+var splat = new Audio("splat.wav");
+var boing = new Audio("boing.wav");
+var clown = new Audio("clown.wav");
+var sneeze = new Audio("sneeze.wav");
+var music = new Audio("IraqLobster.ogg");
+document.addEventListener("keypress", (event) => {
+  const keyName = event.key;
+  if (keyName === "a") {
+    bark.play();
+  } else if (keyName === "s") {
+    whistle.play();
+  } else if (keyName === "d") {
+    splat.play();
+  } else if (keyName === "j") {
+    boing.play();
+  } else if (keyName === "k") {
+    clown.play();
+  } else if (keyName === "l") {
+    sneeze.play();
   } else {
-    alert("Wrong");
   }
+});
+document.addEventListener("keydown", (event) => {
+  const keyName = event.key;
+  if (keyName === "a") {
+    bark.load();
+  } else if (keyName === "s") {
+    whistle.load();
+  } else if (keyName === "d") {
+    splat.load();
+  } else if (keyName === "j") {
+    boing.load();
+  } else if (keyName === "k") {
+    clown.load();
+  } else if (keyName === "l") {
+    sneeze.load();
+  } else {
+  }
+});
+
+function clearA() {
+  lober.style.animation = "dance 0s infinite";
 }
-console.log(num1.innerText);
-function newProblem() {
-  random1 = Math.round(Math.random() * (100 - 1) + 1);
-  random2 = Math.round(Math.random() * (100 - 1) + 1);
-  num1.innerText = random1;
-  num2.innerText = random2;
-}
-var text = document.getElementById("change");
-var color = document.getElementById("color");
-function change() {
-  text.innerText = "Hello Nobody";
-  color.style = "background-color:red;";
+function lobsterm() {
+  music.play();
+  lober.style.animation = "dance 1s 26";
 }
